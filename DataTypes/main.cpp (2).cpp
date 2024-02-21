f5dcd7bb41f6f34f28c7c2eec05c56f22efef3dc
sscanon <160760117+sscanon@users.noreply.github.com>
2024-02-21 11:06:54 -0500

    Add files via upload

---
+++ DataTypes/main.cpp (2).cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+/* run this program using the console pauser or add your own getch, system("pause") or input loop */
+
+int main(int argc, char** argv) {
+	
+	float f1=35e3;
+	double d1=12E4;
+	cout<< f1;
+	cout<< d1;
+	
+	return 0;
+}
